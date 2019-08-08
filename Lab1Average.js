5e1d512de395bf80d8eda475ef213e727fb2f79c
GRodLegacy <vdgonzalez92@yahoo.com>
2019-08-08 15:37:21 -0500

    Add files via upload

---
+++ Lab1Average.js
@@ -0,0 +1,8 @@
+// array average lab
+
+let numbers = [10, 3, 21, 55, 99, 33, 12, 76];
+let average = (numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7])  / 8 ;
+
+console.log(average);
+
+alert("The average of the given numbers: (" + numbers + ") is " + average);
